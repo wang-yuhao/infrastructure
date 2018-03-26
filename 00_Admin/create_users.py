2fbd8828281e6884d6b85205afb617d6aa074a55
Andre Luckow <andre.luckow@gmail.com>
2018-03-26 12:43:03 +0200

    working on details

---
+++ 00_Admin/create_users.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os, sys
+
+start=3
+end=22
+prefix="mnmda"
+directory="/home/hpc/ug201/di57hah/project/students" 
+
+for i in range(start,end):
+    os.makedirs(os.path.join(directory, "mnmda%03d"%i))
+

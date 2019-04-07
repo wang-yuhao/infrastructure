593581f920acbb03fe9ec8b87b51b8ee1b5a26aa
LRZ-SIM Funktionskennung <mnmda001@ivy-login.cos.lrz.de>
2019-04-07 22:08:49 +0200

    making exercises 2019 ready

--- 00_Admin/create_users.py
+++ 00_Admin/create_users.py
@@ -6,6 +6,7 @@ start=3
 end=22
 prefix="mnmda"
 directory="/home/hpc/ug201/di57hah/project/students" 
+directory=os.path.join(os.environ["WORK"], "students")
 
 for i in range(start,end):
     os.makedirs(os.path.join(directory, "mnmda%03d"%i))
